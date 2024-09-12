c4c794871382c869a72d5817c0f9396942caf77a
LAXMI KANT AZAD <37404280+laxmikant16d@users.noreply.github.com>
2024-09-12 22:38:49 +0530

    Updating S3 Bucket Name

--- 04-variables-and-outputs/examples/main.tf
+++ 04-variables-and-outputs/examples/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
+    bucket         = "laxmi-devops-directive-tf-state"
     key            = "04-variables-and-outputs/examples/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
